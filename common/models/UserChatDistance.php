08e8819ba79eb65ba8773c35eea960e7d9cbb0bc
Dev <devp2101@gmail.com>
2022-06-24 13:46:37 +0530

    user-chat-distance model is generated

---
+++ common/models/UserChatDistance.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "user_chat_distance".
+ *
+ * @property int $id
+ * @property float $km_from
+ * @property float $km_to
+ * @property float $min_order_price
+ * @property float $delivery_price
+ * @property int $user_id
+ */
+class UserChatDistance extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'user_chat_distance';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['km_from', 'km_to', 'min_order_price', 'delivery_price', 'user_id'], 'required'],
+            [['km_from', 'km_to', 'min_order_price', 'delivery_price'], 'number'],
+            [['user_id'], 'integer'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'km_from' => 'Km From',
+            'km_to' => 'Km To',
+            'min_order_price' => 'Min Order Price',
+            'delivery_price' => 'Delivery Price',
+            'user_id' => 'User ID',
+        ];
+    }
+}
