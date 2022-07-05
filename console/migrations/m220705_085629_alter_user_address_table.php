178b3a1d7e69a60ea20e3f9eff9903c9fb3e7a6c
Dev <devp2101@gmail.com>
2022-07-05 14:30:12 +0530

    query is added in this file

--- console/migrations/m220705_085629_alter_user_address_table.php
+++ console/migrations/m220705_085629_alter_user_address_table.php
@@ -12,7 +12,10 @@ class m220705_085629_alter_user_address_table extends Migration
      */
     public function safeUp()
     {
-
+        $query =<<<EOF
+ALTER TABLE `user_address` ADD `uudi` VARCHAR(32) NOT NULL AFTER `user_id`; 
+EOF;
+        $this->execute($query);
     }
 
     /**
