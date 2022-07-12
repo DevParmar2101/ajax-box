ca07902e031c1de5880eb9e6c8e4dc837c549d4a
Dev <devp2101@gmail.com>
2022-07-12 19:09:06 +0530

    fixes

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -325,9 +325,11 @@ class SiteController extends Controller
     {
         $model = new ContactForm();
         if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+
             if ($model->sendEmail(Yii::$app->params['adminEmail'])) {
                 Yii::$app->session->setFlash('success', 'Thank you for contacting us. We will respond to you as soon as possible.');
-            } else {
+            }
+            else {
                 Yii::$app->session->setFlash('error', 'There was an error sending your message.');
             }
 
